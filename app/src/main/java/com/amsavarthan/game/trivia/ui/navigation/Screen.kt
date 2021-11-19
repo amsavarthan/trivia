e7fa7538094b843aab7f7509745fa86f5dde58b2
Amsavarthan Lv <80940598+amsavarthan@users.noreply.github.com>
2021-11-19 17:44:55 +0530

    Adds modes configuration screens

--- app/src/main/java/com/amsavarthan/game/trivia/ui/navigation/Screen.kt
+++ app/src/main/java/com/amsavarthan/game/trivia/ui/navigation/Screen.kt
@@ -1,5 +1,8 @@
 package com.amsavarthan.game.trivia.ui.navigation
 
 enum class Screen(val route: String) {
-    HOME("home");
+    CHOOSE_MODE("choose-mode"),
+    QUICK_MODE("quick-mode"),
+    CASUAL_MODE("casual-mode"),
+    DUEL_MODE("duel-mode");
 }
---
+++ app/src/main/java/com/amsavarthan/game/trivia/view/screen/modes/DuelModeConfig.kt
@@ -0,0 +1,25 @@
+package com.amsavarthan.game.trivia.view.screen.modes
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.amsavarthan.game.trivia.ui.theme.AppTheme
+
+@Composable
+fun DuelModeConfig(navController: NavController? = null) {
+
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DuelModeConfigPreview() {
+    AppTheme {
+        Box(modifier = Modifier.fillMaxSize()) {
+            DuelModeConfig()
+        }
+    }
+}
